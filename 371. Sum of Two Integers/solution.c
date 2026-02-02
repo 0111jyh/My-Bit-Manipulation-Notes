db70b9d8414811224524529fba40bbbe1b2940bd
lisa Huang <132056981+0111jyh@users.noreply.github.com>
2026-02-02 14:09:42 +0800

    Enhance comments in solution.c for clarity
    Added comments to explain the logic of the getSum function.

--- 371. Sum of Two Integers/solution.c
+++ 371. Sum of Two Integers/solution.c
@@ -2,6 +2,8 @@
  * Problem: 371. Sum of Two Integers
  * Link: https://leetcode.com/problems/sum-of-two-integers/
  */
+// b != 0 相當於：還有進位的部分需處理
+// 當 b == 0 表示加法已整理完畢，不再產生進位
 int getSum(int a, int b) {
     unsigned int carry;
     //對 signed integer 做 left shift，
